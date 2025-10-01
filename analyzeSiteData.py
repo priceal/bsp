b25d7872f2b0a455bceacbe68fb5189271c626c2
priceal <priceal@emmanuel.edu>
2025-10-01 07:54:43 -0400

    2025 10 01A

---
+++ analyzeSiteData.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+analyze a data file containing cognate site data.
+format should be two columns separated by a delimiter
+    RE Name (1)     site sequence (2)
+    
+output:
+    number of entries
+    site length stats
+    histogram of site lengths
+    histogram of character use in site strings
+"""
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+'''
+###############################################################################
+###############################################################################
+###############################################################################
+'''
+
+# data file name and directory
+siteFile = 'proto_clean.csv'
+dataDir = '/home/allen/projects/DATA/bsp'
+
+# character separating columns
+delimitChar = '\t'
+
+###############################################################################
+################ DOT NOT CHANGE ANYTHING UNDER THIS SEPARATOR #################
+###############################################################################
+
+# read, add length column and print stats
+data=pd.read_csv(os.path.join(dataDir,siteFile),
+                   delimiter=delimitChar,
+                   names=['RE','site'])
+siteLengths =[ len(s) for s in data['site'] ]
+data['length']=pd.Series(siteLengths)
+print(data.describe())
+
+# create site length histogram
+data[['length']].hist(bins=20)
+
+# now create character use list, print
+sitesConcat = ''.join(data.site)
+charSet = set( sitesConcat ).difference({'A','C','G','T','N'})
+charList = list(charSet)
+charList.sort()
+charList = ['A','C','G','T','N'] + charList
+print( 'character set:', charList )
+
+# count character use and plot
+charCounts = []
+for c in charList:
+    charCounts.append( sitesConcat.count(c) )
+plt.figure(2)
+plt.title('character use')
+plt.bar(range(len(charCounts)),charCounts,tick_label=charList) 
